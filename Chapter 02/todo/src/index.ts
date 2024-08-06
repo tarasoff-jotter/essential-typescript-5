01434551282e64c9d8d8a0783d889e9a318f4d66
tarasoff-jotter <155919115+tarasoff-jotter@users.noreply.github.com>
2024-08-07 02:19:46 +0300

    Update index.ts
    change type for variable (collection).

--- Chapter 02/todo/src/index.ts
+++ Chapter 02/todo/src/index.ts
@@ -7,7 +7,7 @@ let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
     new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 
-let collection: TodoCollection = new JsonTodoCollection("Adam", todos);
+let collection: JsonTodoCollection = new JsonTodoCollection("Adam", todos);
 let showCompleted = true;
 
 function displayTodoList(): void {
